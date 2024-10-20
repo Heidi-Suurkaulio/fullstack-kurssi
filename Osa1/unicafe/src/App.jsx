92e1f717ffb2d011821e62c525f47df82591805d
Heidi-Suurkaulio <heidisuur@gmail.com>
2024-10-20 15:40:40 +0300

    tehtava 1.6 asynkronisuus korjataan seuraavassa osassa

--- Osa1/unicafe/src/App.jsx
+++ Osa1/unicafe/src/App.jsx
@@ -1,14 +1,54 @@
 import { useState } from 'react'
 
+const Button = ({ handleClick, text }) => (  
+  <button onClick={handleClick}>    
+    {text}  
+  </button>
+)
+
+const Display = ({good, neutral, bad}) => {
+  return (
+    <div>
+      <p>Statistics</p>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+    </div>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
-  //const [good, setGood] = useState(0)
-  //const [neutral, setNeutral] = useState(0)
-  //const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    //setGood(good + 1)
+    //console.log(good)
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    //console.log(good)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+    //console.log(neutral)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <div>
-      code here
+      <div>
+        <p>give feedback</p>
+      <Button handleClick={handleGood} text = 'good' />        
+      <Button handleClick={handleNeutral} text = 'neutral' />
+      <Button handleClick={handleBad} text = 'bad' />
+      </div>
+      <Display good = {good} neutral = {neutral} bad = {bad} />
     </div>
   )
 }
