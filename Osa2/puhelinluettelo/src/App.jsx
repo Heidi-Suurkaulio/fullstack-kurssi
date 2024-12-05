2877c881fdd6b17349d1fda00079160264ee595e
Heidi-Suurkaulio <heidisuur@gmail.com>
2024-12-05 20:09:10 +0200

    Tehtava 2.6

--- Osa2/puhelinluettelo/src/App.jsx
+++ Osa2/puhelinluettelo/src/App.jsx
@@ -3,22 +3,40 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
+  ])
   const [newName, setNewName] = useState('')
 
+  const handleListChange = (event) => {
+    setNewName(event.target.value)
+  }
+  
+  const addName = (event) => {
+    event.preventDefault()
+    const newPerson = {name: newName}
+    setPersons(persons.concat(newPerson))
+    setNewName("")
+  }
+  
+  //TODO korjaa vaarallinen indeksi
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName}
+          onChange={handleListChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((p, i) =>
+          <li key={i}> {p.name} </li>
+        )
+        }
+      </ul>
     </div>
   )
 
