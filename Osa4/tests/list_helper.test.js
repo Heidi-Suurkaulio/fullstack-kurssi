e22903283acaa0d13439c2d6eb3b492f111f2d38
Heidi-Suurkaulio <heidisuur@gmail.com>
2025-01-20 20:38:19 +0200

    tehtava 4.4

--- Osa4/tests/list_helper.test.js
+++ Osa4/tests/list_helper.test.js
@@ -1,4 +1,4 @@
-const {test, describe} = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
@@ -8,3 +8,60 @@ test('dummy returns one'), () => {
     const result = listHelper.dummy(blogs)
     assert.strictEqual(result, 1)
 }
+
+describe('total likes', () => {
+    const listNoBlogs = []
+
+    const listWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    const listWithManyBlogs = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f7',
+            title: 'The “delay-the-reckoning heuristic” in pro football?',
+            author: 'Andrew Gelman',
+            url: 'https://statmodeling.stat.columbia.edu/',
+            likes: 9,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f6',
+            title: 'Hirmuisten liskojen menneisyys',
+            author: 'Heikki Kämäräinen',
+            url: 'https://www.jyu.fi/fi/blogikirjoitus/hirmuisten-liskojen-menneisyys',
+            likes: 6,
+            __v: 0
+        }
+    ]
+
+    test('of empty list is zero', () => {
+        const result = listHelper.totalLikes(listNoBlogs)
+        assert.strictEqual(result, 0)
+    })
+
+    test('when list has only one blog equals the likes of that', () => {
+        const result = listHelper.totalLikes(listWithOneBlog)
+        assert.strictEqual(result, 5)
+    })
+
+    test('of a bigger list is calculated right', () => {
+        const result = listHelper.totalLikes(listWithManyBlogs)
+        assert.strictEqual(result, 20)
+    })
+})
--- Osa4/utils/list_helper.js
+++ Osa4/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const le = blogs.length
+    
+    if (le < 1) return 0
+
+    const sum = blogs.reduce((accumulator, current) => 
+        accumulator + current.likes, 0)
+    return sum
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
