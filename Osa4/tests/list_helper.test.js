75ff4e91d7a87b6caebc1a77e8b58bb58903b515
Heidi-Suurkaulio <heidisuur@gmail.com>
2025-01-30 15:33:13 +0200

    tehtava 4.6* mostBlogs

--- Osa4/tests/list_helper.test.js
+++ Osa4/tests/list_helper.test.js
@@ -2,56 +2,78 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one'), () => {
-    const blogs = []
+const noBlogs = []
 
-    const result = listHelper.dummy(blogs)
+const listWithOneBlog = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    }
+]
+
+const listWithManyBlogs = [
+    {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0
+    },
+    {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12,
+        __v: 0
+    },
+    {
+        _id: "5a422b891b54a676234d17fa",
+        title: "First class tests",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        likes: 10,
+        __v: 0
+    },
+    {
+        _id: "5a422ba71b54a676234d17fb",
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+        likes: 0,
+        __v: 0
+    },
+    {
+        _id: "5a422bc61b54a676234d17fc",
+        title: "Type wars",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+        likes: 2,
+        __v: 0
+    }
+]
+
+test('dummy returns one'), () => {
+    const result = listHelper.dummy(noBlogs)
     assert.strictEqual(result, 1)
 }
 
 describe('total likes', () => {
-    const listNoBlogs = []
-
-    const listWithOneBlog = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 5,
-            __v: 0
-        }
-    ]
-
-    const listWithManyBlogs = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 5,
-            __v: 0
-        },
-        {
-            _id: '5a422aa71b54a676234d17f7',
-            title: 'The “delay-the-reckoning heuristic” in pro football?',
-            author: 'Andrew Gelman',
-            url: 'https://statmodeling.stat.columbia.edu/',
-            likes: 9,
-            __v: 0
-        },
-        {
-            _id: '5a422aa71b54a676234d17f6',
-            title: 'Hirmuisten liskojen menneisyys',
-            author: 'Heikki Kämäräinen',
-            url: 'https://www.jyu.fi/fi/blogikirjoitus/hirmuisten-liskojen-menneisyys',
-            likes: 6,
-            __v: 0
-        }
-    ]
-
     test('of empty list is zero', () => {
-        const result = listHelper.totalLikes(listNoBlogs)
+        const result = listHelper.totalLikes(noBlogs)
         assert.strictEqual(result, 0)
     })
 
@@ -62,79 +84,54 @@ describe('total likes', () => {
 
     test('of a bigger list is calculated right', () => {
         const result = listHelper.totalLikes(listWithManyBlogs)
-        assert.strictEqual(result, 20)
+        assert.strictEqual(result, 36)
     })
 })
 
 describe('favorite blog', () => {
-    const noBlogs = []
-
-    const listWithOneBlogFav = [
-        {
-            _id: "5a422a851b54a676234d17f7",
-            title: "React patterns",
-            author: "Michael Chan",
-            url: "https://reactpatterns.com/",
-            likes: 7,
-            __v: 0
-        }
-    ]
-
-    const listWithManyBlogsFav = [
-        {
-            _id: "5a422aa71b54a676234d17f8",
-            title: "Go To Statement Considered Harmful",
-            author: "Edsger W. Dijkstra",
-            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-            likes: 5,
-            __v: 0
-        },
-        {
-            _id: "5a422b3a1b54a676234d17f9",
-            title: "Canonical string reduction",
-            author: "Edsger W. Dijkstra",
-            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-            likes: 12,
-            __v: 0
-        },
-        {
-            _id: "5a422b891b54a676234d17fa",
-            title: "First class tests",
-            author: "Robert C. Martin",
-            url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-            likes: 10,
-            __v: 0
-        },
-        {
-            _id: "5a422ba71b54a676234d17fb",
-            title: "TDD harms architecture",
-            author: "Robert C. Martin",
-            url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-            likes: 0,
-            __v: 0
-        }
-    ]
-
     test('of empty list is empty', () => {
         const result = listHelper.favoriteBlog(noBlogs)
         assert.deepEqual(result, {})
     })
 
     test('when the list has one blog return that', () => {
-        const result = listHelper.favoriteBlog(listWithOneBlogFav)
+        const result = listHelper.favoriteBlog(listWithOneBlog)
         assert.deepEqual(result, {
-            title: "React patterns",
-            author: "Michael Chan",
-            likes: 7
+            title: "Go To Statement Considered Harmful",
+            author: "Edsger W. Dijkstra",
+            likes: 5
         })
     })
 
     test('of a bigger list, the right one', () => {
-        const result = listHelper.favoriteBlog(listWithManyBlogsFav)
+        const result = listHelper.favoriteBlog(listWithManyBlogs)
         assert.deepEqual(result, {
             title: "Canonical string reduction",
             author: "Edsger W. Dijkstra",
             likes: 12
+        })
+    })
+})
+
+describe('most blogs', () => {
+    test('of empty list is empty', () => {
+        const result = listHelper.mostBlogs(noBlogs)
+        assert.deepEqual(result, {})
+    })
+
+    test('when the list has one blog return that', () => {
+        const result = listHelper.mostBlogs(listWithOneBlog)
+        assert.deepEqual(result, { 
+            name: 'Edsger W. Dijkstra',
+            blogs: 1
+        })
+    })
+
+    test('of a bigger list, the right one', () => {
+        const result = listHelper.mostBlogs(listWithManyBlogs)
+        assert.deepEqual(result, {
+            name: 'Robert C. Martin',
+            blogs: 3
         })
     })
 })
