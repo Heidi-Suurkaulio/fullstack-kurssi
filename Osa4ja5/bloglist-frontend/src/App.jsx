66afe74f32861c1746c0810eeb969f7864f7ba8b
Heidi-Suurkaulio <heidisuur@gmail.com>
2025-03-20 14:28:12 +0200

    tehtava 5.7

--- Osa4ja5/bloglist-frontend/src/App.jsx
+++ Osa4ja5/bloglist-frontend/src/App.jsx
@@ -97,7 +97,7 @@ const App = () => {
         </Togglable>
         <h2>Blogs</h2>
         {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
+          <Blog key={blog.id} blog={blog} user={user} />
         )}
       </div>
       }
--- Osa4ja5/bloglist-frontend/src/components/Blog.jsx
+++ Osa4ja5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,56 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog, user }) => {
+  const [showAll, setShowAll] = useState(false)
+
+  const hideDetails = {
+    border: '0.1em solid black',
+    padding: '0.25em',
+    margin: '0.25em',
+    display: showAll ? 'none' : ''
+  }
+  const showDetails = {
+    border: '0.1em solid black',
+    padding: '0.25em',
+    margin: '0.25em',
+    display: showAll ? '' : 'none'
+  }
+
+  const toggleShowAll = () => {
+    setShowAll(!showAll)
+  }
+
+  return <div>
+    <div style={hideDetails}>
+      <strong>{blog.title}</strong> {blog.author}
+      <button onClick={toggleShowAll} style={{float: 'right'}}>
+        Show Details
+      </button>
+    </div>
+    <div style={showDetails}>
+      <ul style={{ listStyleType: 'none' }}>
+        <li>
+          <strong>{blog.title}</strong>
+        </li>
+        <li>
+          {blog.author}
+        </li>
+        <li>
+          <a href={blog.url}>{blog.url}</a>
+        </li>
+        <li>            
+          likes: {blog.likes} 
+          <button> Like </button>
+        </li>
+        <li>
+          {user.name}
+        </li>
+      </ul>
+      <button onClick={toggleShowAll}>
+        Hide Details
+      </button>
+    </div>
+  </div>
+}
 
 export default Blog
