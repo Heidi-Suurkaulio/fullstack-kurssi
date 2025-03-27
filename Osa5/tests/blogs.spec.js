0d3eaaca7c54ac7c47bb75d086fbe90d48c81b43
Heidi-Suurkaulio <heidisuur@gmail.com>
2025-03-27 21:55:01 +0200

    tehtava 5.18

--- Osa5/tests/blogs.spec.js
+++ Osa5/tests/blogs.spec.js
@@ -20,4 +20,22 @@ describe('Blog app', () => {
         const fields = await page.getByRole('textbox')
         await expect(fields).toHaveCount(2)
     })
+
+    describe('Login', () => {
+        test('succeeds with correct credentials', async ({ page }) => {
+            await page.getByRole('textbox').first().fill('mluukkai')
+            await page.getByRole('textbox').last().fill('salainen')
+            await page.getByRole('button', { name: 'Log In' }).click()
+
+            await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+        })
+    
+        test('fails with wrong credentials', async ({ page }) => {
+            await page.getByRole('textbox').first().fill('mluukkai')
+            await page.getByRole('textbox').last().fill('väärä')
+            await page.getByRole('button', { name: 'Log In' }).click()
+
+            await expect(page.getByText('Log in to application')).toBeVisible()
+        })
+    })
 })
